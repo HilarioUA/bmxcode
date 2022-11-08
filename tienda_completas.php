cc6fc2f33c0c8422beb324409321f6f4b98ac35c
HilarioUA <josexd303@gmail.com>
2022-11-08 15:44:45 -0700

    eventos

---
+++ tienda_completas.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Piezas de bicicleta</title>
+    <link rel="stylesheet" href="css/estilos.css?a=1">
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css" crossorigin="anonymous">
+    <script src="css/bootstrap/js/bootstrap.min.js" crossorigin="anonymous"></script>
+    <?php include 'partes/cabecera.php'; ?>
+</head>
+<body>
+    <div class="contenido_todo_piezas row">
+        <div class="contenido_piezas col-md-3">
+            <img src="img/primo.jpg" width="180" height="180">
+            <div class="nombre_costo">
+                <p>Primo <br> $16,599</p>
+            </div>
+        </div>
+        <div class="contenido_piezas col-md-3">
+            <img src="img/fiend.jpg" width="180" height="180">
+            <div class="nombre_costo">
+                <p>Fiend <br> $21,819</p>
+            </div>
+        </div>
+        <div class="contenido_piezas col-md-3">
+            <img src="img/tornasol.jpg" width="180" height="180">
+            <div class="nombre_costo">
+                <p>Tornasol <br> $15,198</p>
+            </div>
+        </div>
+        <div class="contenido_piezas col-md-3">
+            <img src="img/stolen.jpg" width="180" height="180">
+            <div class="nombre_costo">
+                <p>Stolen <br> $12,299</p>
+            </div>
+        </div>
+        <div class="contenido_piezas col-md-3">
+            <img src="img/mongoose.jpg" width="140" height="180">
+            <div class="nombre_costo">
+                <p>Mongoose <br> $8,198</p>
+            </div>
+        </div>
+        <div class="contenido_piezas col-md-3">
+            <img src="img/colony.jpg" width="180" height="180">
+            <div class="nombre_costo">
+                <p>Colony <br> $20,129</p>
+            </div>
+        </div>
+        <div class="contenido_piezas col-md-3">
+            <img src="img/destro.jpg" width="180" height="180">
+            <div class="nombre_costo">
+                <p>Destro Elite <br> $10,599</p>
+            </div>
+        </div>
+        <div class="contenido_piezas col-md-3">
+            <img src="img/cult.jpg" width="180" height="180">
+            <div class="nombre_costo">
+                <p>Cult <br> $18,999</p>
+            </div>
+        </div>
+    </div>
+
+</body>
+<?php include 'partes/pie.php'; ?>
+</html>
---
+++ tienda_piezas.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Piezas de bicicleta</title>
+    <link rel="stylesheet" href="css/estilos.css?a=3">
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css" crossorigin="anonymous">
+    <script src="css/bootstrap/js/bootstrap.min.js" crossorigin="anonymous"></script>
+    <?php include 'partes/cabecera.php'; ?>
+</head>
+<body>
+    <div class="contenido_todo_piezas row">
+        <div class="contenido_piezas col-md-3">
+            <img src="img/plato_mision.jpg" width="130" height="130">
+            <div class="nombre_costo">
+                <p>Mision <br> $699</p>
+            </div>
+        </div>
+        <div class="contenido_piezas col-md-3">
+            <img src="img/maza_salt.jpg" width="130" height="130">
+            <div class="nombre_costo">
+                <p>Maza Salt <br> $2,650</p>
+            </div>
+        </div>
+        <div class="contenido_piezas col-md-3">
+            <img src="img/asiento_shadow.jpg" width="130" height="130">
+            <div class="nombre_costo">
+                <p>Shadow <br> $900</p>
+            </div>
+        </div>
+        <div class="contenido_piezas col-md-3">
+            <img src="img/flybikes.jpg" width="130" height="130">
+            <div class="nombre_costo">
+                <p>FlyBikes <br> $4,998</p>
+            </div>
+        </div>
+
+        <div class="contenido_piezas col-md-3">
+            <img src="img/azul.jpg" width="130" height="130">
+            <div class="nombre_costo">
+                <p>VG <br> $599</p>
+            </div>
+        </div>
+        <div class="contenido_piezas col-md-3">
+            <img src="img/federal.jpg" width="130" height="130">
+            <div class="nombre_costo">
+                <p>Federal <br> $649</p>
+            </div>
+        </div>
+        <div class="contenido_piezas col-md-3">
+            <img src="img/vans.jpg" width="130" height="130">
+            <div class="nombre_costo">
+                <p>Vans <br> $999</p>
+            </div>
+        </div>
+        <div class="contenido_piezas col-md-3">
+            <img src="img/c_primo.jpg" width="130" height="130">
+            <div class="nombre_costo">
+                <p>Primo <br> $698</p>
+            </div>
+        </div>
+    </div>
+
+</body>
+<?php include 'partes/pie.php'; ?>
+</html>

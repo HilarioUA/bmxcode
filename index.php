a13f20cb46cee1526423fcfdf1edb5daabd78007
HilarioUA <josexd303@gmail.com>
2022-11-08 14:21:47 -0700

    Comienzo

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página principal</title>
+    <link rel="stylesheet" href="./css/estilos.css">
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css" crossorigin="anonymous">
+    <script src="css/bootstrap/js/bootstrap.min.js" crossorigin="anonymous"></script>
+
+    <?php include('partes/cabecera.php'); ?>
+</head>
+
+<body>
+
+</body>
+</html>
